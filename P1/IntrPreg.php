a9cfbc336917cc00a5822f16931bd5799dbb5e16
Jesús Márquez <jesusmarquezdelgado@gmail.com>
2020-03-10 17:39:19 +0100

    Proyectos

--- P1/IntrPreg.php
+++ P1/IntrPreg.php
@@ -12,11 +12,11 @@
 		$db = 'p1';
 		$port = '3308';
 		$conexion = mysqli_connect($dbhost,$dbuser,$dbpass,$db,$port) or die ("No se pudo establecer conexion con el servidor");
-		mysqli_query($conexion,"INSERT INTO pregunta (cod_preg,enunciado) VALUES (NULL,'hola')") or die ("Fallo Introducir datos a tabla");
+		mysqli_query($conexion,"INSERT INTO pregunta (cod_preg,enunciado) VALUES (NULL,'".$_POST['res']."')") or die ("Fallo Introducir datos a tabla");
 	}
 	?>
 	<form method="post" action="IntrPreg.php">
-	Enviar Pregunta: <input type="text" name="res">
+	Enviar Pregunta: <input type="text" name="res" required>
 	</form>
 </body>
 </html>
