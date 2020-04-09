95f85412c63a78672d5844e7bed74ad31360d2b9
Kaiser16 <jesusmarquezdelgado@gmail.com>
2020-04-09 05:16:00 +0200

    Proyectos

--- P1/Resultados.php
+++ P1/Resultados.php
@@ -1,11 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Resultados</title>
-</head>
-
-<body>
-	<?php
+<?php
 	error_reporting(E_ERROR | E_PARSE);
 	$dbhost = '127.0.0.1';
 	$dbuser = 'root';
@@ -21,6 +14,7 @@
 	$cProfesor = $_POST['cProfesor'];
 	$profesor = $_POST['profesor'];
 
+	$tEdad = "Edad";
 	$cEdad = $_POST['cEdad'];
 	$edad = $_POST['edad'];
 	$valorEdades = array("1" => "<=19", "2" => "20-21", "3" => "21-23", "4" => "
@@ -60,145 +54,47 @@
 
 	$cCalificacion = $_POST['cCalificacion'];
 	$calificacion = $_POST['calificacion'];
-	$valorCalificacion = array("1" => "N.P.", "2" => "Sus.", "3" => "Apro.", "4" => "Not.", "5" => "Sobr.", "6" => "Mat.Hon.");
+	$valorCalificacion = array("1" => "No Presentado", "2" => "Suspenso", "3" => "Aprobado", "4" => "Notable", "5" => "Sobresaliente", "6" => "Matricula de Honor");
 
 	$cAsistencia = $_POST['cAsistencia'];
 	$asistencia = $_POST['asistencia'];
 	$valorAsistencia = array("1" => "Menos 50%", "2" => "Entre 50% y 80%", "3" => "Más de 80%");
 
-	$valorPreguntas = array("0"=> "NS", "1" => "1", "2" => "2", "3" => "3", "4" => "
+	$valorPreguntas = array("0"=> "N.S.", "1" => "1", "2" => "2", "3" => "3", "4" => "
 		4", "5" => "5");
-	?>
-	<h2>Filtros:</h2>
-	<form action="Resultados.php" method="post">
+?>
 
-		<!--<input type="checkbox" name="cPregunta">-->
-		Pregunta: <select name=pregunta>
-		<option value="-1">Todas</option>
-		<?php 
-		$query = $conexion->query("Select * from pregunta") or die("Fallo consulta");
-		while ($row = $query->fetch_assoc()):
-			echo "<option value=".$row['cod_preg'].">".$row['enunciado']."</option>";
-		endwhile;
-		?>
-		</select>
-		<br>
+<?php
 
-		<input type="checkbox" name="cProfesor">
-		Profesor: <select name=profesor>
-		<?php 
-		$query = $conexion->query("Select * from profesor") or die("Fallo consulta");
-		while ($row = $query->fetch_assoc()):
-			echo "<option value=".$row['cod_prof'].">".$row['cod_prof']."</option>";
-		endwhile; 
-		?>
-		</select>
-		<br>
+function insertarWhereName(&$wherecond,&$where,$value,$id,$type,$string,&$name)
+{
+	if($wherecond == "true")
+		$where = $where." and ";
+	else
+		$wherecond = "true";
+	$where = $where."$type = $id";
+	$name = $name." | $string: ".$value[$id];
+}
+function insertarWhere(&$wherecond,&$where,$id,$type)
+{
+	if($wherecond == "true")
+		$where = $where." and ";
+	else
+		$wherecond = "true";
+	$where = $where."$type = $id";
+}
 
-		<input type="checkbox" name="cEdad">
-		Edad: 
-		<select name="edad">
-			<?php for($i=1; $i <= count($valorEdades); $i++) {
-				echo "<option value=".$i.">".$valorEdades[$i]."</option>";
-			} ?>
-		</select>
-		<br>
+?>
 
-		<input type="checkbox" name="cSexo">
-		Sexo: 
-		<select name="sexo">
-			<?php for($i=1; $i <= count($valorSexo); $i++) {
-				echo "<option value=".$i.">".$valorSexo[$i]."</option>";
-			} ?>
-		</select>
-		<br>
+<?php
+	$puedeMostrar = "true";
+	$buscar = $_POST['Buscar'];
+	if(isset($buscar))
+		$cBuscar = "true";
+?>
 
-		<input type="checkbox" name="cAlto">
-		Curso más alto:
-		<select name="alto">
-			<?php for($i=1; $i <= count($valorAlto); $i++) {
-				echo "<option value=".$i.">".$valorAlto[$i]."</option>";
-			} ?>
-		</select>
-		<br>
-
-		<input type="checkbox" name="cBajo">
-		Curso más bajo:
-		<select name="bajo">
-			<?php for($i=1; $i <= count($valorBajo); $i++) {
-				echo "<option value=".$i.">".$valorBajo[$i]."</option>";
-			} ?>
-		</select>
-		<br>
-
-		<input type="checkbox" name="cMatriculado">
-		Veces matriculado:
-		<select name="matriclado">
-			<?php for($i=1; $i <= count($valorMatriculado); $i++) {
-				echo "<option value=".$i.">".$valorMatriculado[$i]."</option>";
-			} ?>
-		</select>
-		<br>
-
-		<input type="checkbox" name="cExaminado">
-		Veces examinado:
-		<select name="examinado">
-			<?php for($i=1; $i <= count($valorExaminado); $i++) {
-				echo "<option value=".$i.">".$valorExaminado[$i]."</option>";
-			} ?>
-		</select>
-		<br>
-
-		<input type="checkbox" name="cInteres">
-		Interes:
-		<select name="interes">
-			<?php for($i=1; $i <= count($valorInteres); $i++) {
-				echo "<option value=".$i.">".$valorInteres[$i]."</option>";
-			} ?>
-		</select>
-		<br>
-
-		<input type="checkbox" name="cTutorias">
-		Uso de Tutorias:
-		<select name="tutorias">
-			<?php for($i=1; $i <= count($valorTutorias); $i++) {
-				echo "<option value=".$i.">".$valorTutorias[$i]."</option>";
-			} ?>
-		</select>
-		<br>
-
-		<input type="checkbox" name="cDificultad">
-		Dificultad:
-		<select name="dificultad">
-			<?php for($i=1; $i <= count($valorDificultad); $i++) {
-				echo "<option value=".$i.">".$valorDificultad[$i]."</option>";
-			} ?>
-		</select>
-		<br>
-
-		<input type="checkbox" name="cCalificacion">
-		Calificacion esperada:
-		<select name="calificacion">
-			<?php for($i=1; $i <= count($valorCalificacion); $i++) {
-				echo "<option value=".$i.">".$valorCalificacion[$i]."</option>";
-			} ?>
-		</select>
-		<br>
-
-		<input type="checkbox" name="cAsistencia">
-		Asistencia:
-		<select name="asistencia">
-			<?php for($i=1; $i <= count($valorAsistencia); $i++) {
-				echo "<option value=".$i.">".$valorAsistencia[$i]."</option>";
-			} ?>
-		</select>
-		<br>
-
-		<input type="submit" name="Buscar" value="Buscar">
-	</form>
-
-	<?php
-
+<?php
+if($cBuscar == "true"):
 	if($pregunta == "-1")
 		$nombrePregunta = "Todas";
 	else
@@ -228,114 +124,37 @@
 		$id_enQuery = "select id_en from ".$tabla;
 
 		if($cEdad=="on")
-		{
-			if($wherecond == "true")
-				$where = $where." and ";
-			else
-				$wherecond = "true";
-			$where = $where."edad = $edad";
-			$name = $name." | Edad: ".$valorEdades[$edad];
-		}
+			insertarWhereName($wherecond,$where,$valorEdades,$edad,"edad","Edad",$name);
 
 		if($cSexo=="on")
-		{
-			if($wherecond == "true")
-				$where = $where." and ";
-			else
-				$wherecond = "true";
-			$where = $where."sexo = $sexo";
-			$name = $name." | Sexo: ".$valorSexo[$sexo];
-		}
+			insertarWhereName($wherecond,$where,$valorSexo,$sexo,"sexo","Sexo",$name);
 
 		if($cAlto=="on")
-		{
-			if($wherecond == "true")
-				$where = $where." and ";
-			else
-				$wherecond = "true";
-			$where = $where."curso_sup = $alto";
-			$name = $name." | Curso más alto: ".$valorAlto[$alto];
-		}
+			insertarWhereName($wherecond,$where,$valorAlto,$alto,"curso_sup","Curso más alto",$name);
 
 		if($cBajo=="on")
-		{
-			if($wherecond == "true")
-				$where = $where." and ";
-			else
-				$wherecond = "true";
-			$where = $where."curso_inf = $bajo";
-			$name = $name." | Curso más bajo: ".$valorBajo[$bajo];
-		}
+			insertarWhereName($wherecond,$where,$valorBajo,$bajo,"curso_inf","Curso más bajo",$name);
 
 		if($cMatriculado=="on")
-		{
-			if($wherecond == "true")
-				$where = $where." and ";
-			else
-				$wherecond = "true";
-			$where = $where."n_matri = $matriclado";
-			$name = $name." | Veces matriclado: ".$valorMatriculado[$matriclado];
-		}
+			insertarWhereName($wherecond,$where,$valorMatriculado,$matriclado,"n_matri","Veces matriculado",$name);
 
 		if($cExaminado=="on")
-		{
-			if($wherecond == "true")
-				$where = $where." and ";
-			else
-				$wherecond = "true";
-			$where = $where."n_exam = $examinado";
-			$name = $name." | Veces examinado: ".$valorExaminado[$examinado];
-		}
+			insertarWhereName($wherecond,$where,$valorExaminado,$examinado,"n_exam","Veces examinado",$name);
 
 		if($cInteres=="on")
-		{
-			if($wherecond == "true")
-				$where = $where." and ";
-			else
-				$wherecond = "true";
-			$where = $where."interes = $interes";
-			$name = $name." | Interes: ".$valorInteres[$interes];
-		}
+			insertarWhereName($wherecond,$where,$valorInteres,$interes,"interes","Interes",$name);
 
 		if($cTutorias=="on")
-		{
-			if($wherecond == "true")
-				$where = $where." and ";
-			else
-				$wherecond = "true";
-			$where = $where."tutorias = $tutorias";
-			$name = $name." | Tutorias: ".$valorTutorias[$tutorias];
-		}
+			insertarWhereName($wherecond,$where,$valorTutorias,$tutorias,"tutorias","Tutorias",$name);
 
 		if($cDificultad=="on")
-		{
-			if($wherecond == "true")
-				$where = $where." and ";
-			else
-				$wherecond = "true";
-			$where = $where."dificultad = $dificultad";
-			$name = $name." | Dificultad: ".$valorDificultad[$dificultad];
-		}
+			insertarWhereName($wherecond,$where,$valorDificultad,$dificultad,"dificultad","Dificultad",$name);
 
 		if($cCalificacion=="on")
-		{
-			if($wherecond == "true")
-				$where = $where." and ";
-			else
-				$wherecond = "true";
-			$where = $where."calif = $calificacion";
-			$name = $name." | Calificacion: ".$valorCalificacion[$calificacion];
-		}
+			insertarWhereName($wherecond,$where,$valorCalificacion,$calificacion,"calif","Calificacion",$name);
 
 		if($cAsistencia=="on")
-		{
-			if($wherecond == "true")
-				$where = $where." and ";
-			else
-				$wherecond = "true";
-			$where = $where."asist = $asistencia";
-			$name = $name." | Asistencia: ".$valorAsistencia[$asistencia];
-		}
+			insertarWhereName($wherecond,$where,$valorAsistencia,$asistencia,"asist","Asistencia",$name);
 
 		if($wherecond == "true")
 			$id_enQuery = $id_enQuery.$where.")";
@@ -353,21 +172,11 @@
 	$tabla = "respuesta";
 	$Query = "select ".$select." from ".$tabla;
 
-	if($pregunta != "-1"):
-		if($wherecond == "true")
-			$where = $where." and ";
-		else
-			$wherecond = "true";
-		$where = $where."cod_preg = $pregunta";
-	endif;
+	if($pregunta != "-1")
+		insertarWhere($wherecond,$where,$pregunta,"cod_preg",);
 
-	if($cProfesor == "on"):
-		if($wherecond == "true")
-			$where = $where." and ";
-		else
-			$wherecond = "true";
-		$where = $where."cod_prof = $profesor";
-	endif;
+	if($cProfesor == "on")
+		insertarWhere($wherecond,$where,$profesor,"cod_prof",);
 
 	if($QEncuesta == "on"):
 		if($wherecond == "true")
@@ -379,8 +188,7 @@
 
 	if($wherecond == "true")
 		$Query = $Query.$where.")";
-
-	echo $Query;
+	//echo $Query;
 	$respuestas = $conexion->query("$Query") or die("Fallo");
 	$nrespuestas = $respuestas->num_rows;
 
@@ -417,31 +225,10 @@
 		$array = array("y"=> $valorOpcion[$i], "label"=> $valorPreguntas[$i]);
 		array_push($dataPoints,$array);
 	}
+endif;
+?>
 
-	?>
-
-	<script>
-	window.onload = function () {
-	 
-	var chart = new CanvasJS.Chart("chartContainer", {
-		exportEnabled: true,
-		animationEnabled: true,
-		title:{
-			text: "Simple Column Chart with Index Labels"
-		},
-		data: [{
-			type: "pie",
-			dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
-		}]
-	});
-	chart.render();
-	chart.title.set("text", "<?php echo $name ?>");
-	}
-	</script>
-
-	<div id="chartContainer" style="height: 370px; width: 100%;"></div>
-	<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
-	<?php
+<?php
 	//media
 	$media = 0;
 	for($i = 1; $i <= 5; $i++)
@@ -471,9 +258,209 @@
 			$nrepeticiones = $valorOpcion[$i];
 		}
 	}
-	?>
-	<h1 align="center">Media: <?php echo $media; ?></h1>
-	<h1 align="center">Mediana: <?php echo $mediana; ?></h1>
-	<h1 align="center">Moda: <?php echo $moda; ?></h1>
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Resultados</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<style>
+	* {
+	  box-sizing: border-box;
+	}
+
+	/* Create two equal columns that floats next to each other */
+	.column {
+	  float: left;
+	  width: 50%;
+	  padding: 10px;
+	  height: 300px; /* Should be removed. Only for demonstration */
+	}
+
+	/* Clear floats after the columns */
+	.row:after {
+	  content: "";
+	  display: table;
+	  clear: both;
+	}
+	</style>
+	<script>
+		window.onload = function () {
+		 
+		var chart = new CanvasJS.Chart("chartContainer", {
+			exportEnabled: true,
+			animationEnabled: true,
+			theme: "dark1",
+			data: [{
+				type: "pie",
+				showInLegend: "true",
+				indexLabelFontSize: 14,
+				indexLabel: "{y}",
+				legendText: "{label}",
+				indexLabelPlacement: "inside",
+				indexLabelFontColor: "#000000",
+				indexLabelFontWeight: "bolder",
+				dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
+			}]
+		});
+		chart.render();
+		}
+	</script>
+
+	<style type="text/css">
+		body{
+			background: lightgrey;
+			background-image: url('image.jpg');
+			color: white;
+		}
+	</style>
+</head>
+
+<body>
+
+<div class="row">
+  <div class="column">
+		<h2>Filtros:</h2>
+		<form action="Resultados.php" method="post">
+
+			<!--<input type="checkbox" name="cPregunta">-->
+			Pregunta: <select name=pregunta>
+			<option value="-1">Todas</option>
+			<?php 
+			$query = $conexion->query("Select * from pregunta") or die("Fallo consulta");
+			while ($row = $query->fetch_assoc()):
+				echo "<option value=".$row['cod_preg'].">".$row['enunciado']."</option>";
+			endwhile;
+			?>
+			</select>
+			<br>
+
+			<input type="checkbox" name="cProfesor">
+			Profesor: <select name=profesor>
+			<?php 
+			$query = $conexion->query("Select * from profesor") or die("Fallo consulta");
+			while ($row = $query->fetch_assoc()):
+				echo "<option value=".$row['cod_prof'].">".$row['cod_prof']."</option>";
+			endwhile; 
+			?>
+			</select>
+			<br>
+
+			<input type="checkbox" name="cEdad">
+			Edad: 
+			<select name="edad">
+				<?php for($i=1; $i <= count($valorEdades); $i++) {
+					echo "<option value=".$i.">".$valorEdades[$i]."</option>";
+				} ?>
+			</select>
+			<br>
+
+			<input type="checkbox" name="cSexo">
+			Sexo: 
+			<select name="sexo">
+				<?php for($i=1; $i <= count($valorSexo); $i++) {
+					echo "<option value=".$i.">".$valorSexo[$i]."</option>";
+				} ?>
+			</select>
+			<br>
+
+			<input type="checkbox" name="cAlto">
+			Curso más alto:
+			<select name="alto">
+				<?php for($i=1; $i <= count($valorAlto); $i++) {
+					echo "<option value=".$i.">".$valorAlto[$i]."</option>";
+				} ?>
+			</select>
+			<br>
+
+			<input type="checkbox" name="cBajo">
+			Curso más bajo:
+			<select name="bajo">
+				<?php for($i=1; $i <= count($valorBajo); $i++) {
+					echo "<option value=".$i.">".$valorBajo[$i]."</option>";
+				} ?>
+			</select>
+			<br>
+
+			<input type="checkbox" name="cMatriculado">
+			Veces matriculado:
+			<select name="matriclado">
+				<?php for($i=1; $i <= count($valorMatriculado); $i++) {
+					echo "<option value=".$i.">".$valorMatriculado[$i]."</option>";
+				} ?>
+			</select>
+			<br>
+
+			<input type="checkbox" name="cExaminado">
+			Veces examinado:
+			<select name="examinado">
+				<?php for($i=1; $i <= count($valorExaminado); $i++) {
+					echo "<option value=".$i.">".$valorExaminado[$i]."</option>";
+				} ?>
+			</select>
+			<br>
+
+			<input type="checkbox" name="cInteres">
+			Interes:
+			<select name="interes">
+				<?php for($i=1; $i <= count($valorInteres); $i++) {
+					echo "<option value=".$i.">".$valorInteres[$i]."</option>";
+				} ?>
+			</select>
+			<br>
+
+			<input type="checkbox" name="cTutorias">
+			Uso de Tutorias:
+			<select name="tutorias">
+				<?php for($i=1; $i <= count($valorTutorias); $i++) {
+					echo "<option value=".$i.">".$valorTutorias[$i]."</option>";
+				} ?>
+			</select>
+			<br>
+
+			<input type="checkbox" name="cDificultad">
+			Dificultad:
+			<select name="dificultad">
+				<?php for($i=1; $i <= count($valorDificultad); $i++) {
+					echo "<option value=".$i.">".$valorDificultad[$i]."</option>";
+				} ?>
+			</select>
+			<br>
+
+			<input type="checkbox" name="cCalificacion">
+			Calificacion esperada:
+			<select name="calificacion">
+				<?php for($i=1; $i <= count($valorCalificacion); $i++) {
+					echo "<option value=".$i.">".$valorCalificacion[$i]."</option>";
+				} ?>
+			</select>
+			<br>
+
+			<input type="checkbox" name="cAsistencia">
+			Asistencia:
+			<select name="asistencia">
+				<?php for($i=1; $i <= count($valorAsistencia); $i++) {
+					echo "<option value=".$i.">".$valorAsistencia[$i]."</option>";
+				} ?>
+			</select>
+			<br>
+
+			<input type="submit" name="Buscar" value="Buscar">
+		</form>
+	</div>
+ 	<div class="column">
+ 	<?php if($puedeMostrar == "true"): ?>
+ 		<h3 align="center"><?php echo $name; ?></h3>
+		<div id="chartContainer" style="height: 250px; width: 100%;"></div>
+		<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+
+		<h3 align="center">Media: <?php echo $media; ?></h3>
+		<h3 align="center">Mediana: <?php echo $mediana; ?></h3>
+		<h3 align="center">Moda: <?php echo $moda; ?></h3>
+	<?php endif; 
+	$conexion->close();?>
+	</div>
+</div>
 </body>
 </html>  
